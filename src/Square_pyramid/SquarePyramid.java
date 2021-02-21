3cdb630e2ff6dcb55ffba6e34dfecf2202875cd4
anshumansingh005 <anshumansingh005@gmail.com>
2021-02-22 00:37:36 +0530

    Initial commit

---
+++ src/Square_pyramid/SquarePyramid.java
@@ -0,0 +1,22 @@
+package Square_pyramid;
+import java.io.Scanner;
+
+public class SquarePyramid{
+	public static void printnums(int n){
+		int i, j,num;
+		for(i=0; i<n; i++){
+			num=1;
+			for(j=0; j<=i; j++){
+				System.out.print(num*num+ " ");
+				num++;
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		printnums(n);
+	}
+}
+
