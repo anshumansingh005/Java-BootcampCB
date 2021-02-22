5316df8af8ca5f2af0d0c8ea34f567324d20d277
anshumansingh005 <anshumansingh005@gmail.com>
2021-02-22 11:37:06 +0530

    Initial commit

--- src/Hollow_diamond_pattern/HollowDiamond.java
+++ src/Hollow_diamond_pattern/HollowDiamond.java
@@ -1,50 +1,41 @@
 import java.util.Scanner;
 class Main{
 public static void main(String args[]){
-int i,j;
-Scanner scan=new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+   int nst = (n/2) + 1;
+    int nsp = -1;
+    int rows = 1;
+    while(rows <= n){
+      int cst =1;
+      while(cst <= nst){
+        System.out.print("*\t");
+        cst++;
+      }
+      int csp =1;
+        while(csp<= nsp){
+          System.out.print(" \t");
+          csp++;
+        }
 
-int rows=scan.nextInt();
-i=1; 
-while(i<=rows){
-j=i; 
-    while(j<=rows){
-       System.out.print("*");
-        j++;
+      cst = 1;
+      if(rows == 1 || rows == n)
+		 	cst = 2;
+
+        while(cst <= nst){
+          System.out.print("*\t");
+          cst++;
+        }
+         if(rows <= n/2){ 
+            nsp += 2;
+            nst--;
+        }
+		else{
+            nsp -= 2;
+            nst++;
+        }
+        rows++;
+        System.out.println();
     }
-j=1; 
-    while(j<=(2*i-2)){
-       System.out.print(" ");
-        j++;
-    }
-    j=i; 
-    while(j<=rows){
-       System.out.print("*");
-        j++;
-    }
-    System.out.print("\n");
-     i++;
+  }
 }
-    
-    i=1; 
-while(i<=rows){
-j=1; 
-    while(j<=i){
-       System.out.print("*");
-        j++;
-    }
-    j=(2*i-2); 
-    while(j<(2*rows-2)){
-       System.out.print(" ");
-        j++;
-    }
-    j=1; 
-    while(j<=i){
-       System.out.print("*");
-        j++;
-    }
-    System.out.print("\n");
-     i++;
-    }
-    }
-    }
