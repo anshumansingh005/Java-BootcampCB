967e59468057cb2da409c4fa42e1b9e2a52ffd59
anshumansingh005 <anshumansingh005@gmail.com>
2021-03-18 09:29:22 +0530

    Indentation changes

--- src/Count_Palindrome_Substring/palindrome_substring.java
+++ src/Count_Palindrome_Substring/palindrome_substring.java
@@ -9,15 +9,14 @@ class palindrome_substring{
 
   public static int palindrome(String str){
     int count = 0;
-    for(int axis = 0; axis< str.length(); axis ++){
-      for(int orbit = 0; axis-orbit >=0 && orbit+axis <str.length(); orbit++){
-        if(str.charAt(axis-orbit) == str.charAt(axis+orbit))
+    for(int axis = 0; axis< str.length(); axis ++) {
+      for (int orbit = 0; axis - orbit >= 0 && orbit + axis < str.length(); orbit++) {
+        if (str.charAt(axis - orbit) == str.charAt(axis + orbit))
           count++;
-        else 
+        else
           break;
       }
     }
-    
     for(double axis = 0.5; axis<str.length(); axis++){
       for(double orbit = 0.5; axis-orbit>=0&& axis+orbit<str.length();orbit++){
       if(str.charAt((int)(axis -orbit))==str.charAt((int)(axis+orbit)))
